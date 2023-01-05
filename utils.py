ad1d0dc6c9856c3df6c2a0c6717f286a49735b7e
Himanshu Solanki <hsolanki@ad.base-fx.com>
2023-01-05 15:24:01 +0800

    updated path

--- utils.py
+++ utils.py
@@ -5,17 +5,16 @@ import tempfile
 import hou
 
 def getOverlayDir():
-    if hou.hipFile().isNewFile():
+    dirName = str(uuid.uuid1())
+    if hou.hipFile.isNewFile():
         # get the temp dir path
-        return tempfile.gettempdir()
+        dirPath = '{}/{}'.format(tempfile.gettempdir(), dirName)
+         
     else:
         # get the path to the Houdini hipfile folder
         hipfileFolder = hou.getenv('HIP')
-        # generate a new UUID and use it as the name of a directory in the Houdini hipfile folder
-        dirName = str(uuid.uuid4())
-        dirPath = Path(hipfileFolder) / dirName
+        dirPath = '{}/{}'.format(hipfileFolder, dirName)
         # create the directory
-        dirPath.mkdir(exist_ok=True)
-        return str(dirPath)
-
+    os.mkdir(dirPath)
+    return str(dirPath)
 
