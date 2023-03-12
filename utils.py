aff08353672eb83df65fd306636f35894bd1563f
Himanshu Solanki <umt157@gmail.com>
2023-03-12 19:07:01 +0800

    add python sop test run

--- utils.py
+++ utils.py
@@ -43,3 +43,70 @@ if os.path.exists(imgpath):
 cv2.imwrite(imgpath,image)
 
 
+#-------------------------python sop test code ------------------
+import os
+frame = $F
+
+import sys
+sys.path.append("/opt/homebrew/lib/python3.11/site-packages/cv2")
+import cv2
+import uuid
+import numpy as np
+import os
+import hudinfo
+import hou
+node = hou.node("../../")
+print(node.path())
+import importlib
+importlib.reload(hudinfo)
+
+print(node.evalParm("hudCamera"))
+objNode = hou.node(node.evalParm("solverObject"))
+solverNode = hou.node(node.evalParm("solverNode"))
+hudCamera = node.parm("hudCamera").evalAsNode()
+print(hudCamera)
+hudObject = hudinfo.getData(hudCamera=hudCamera, objectNode=objNode, solverNode=solverNode)
+
+    
+
+
+print("start")
+data = (hudObject.get_parm_value(hudObject.pyro_sim_parm))
+#print(data)
+
+print(frame)
+print(hou.pwd())
+
+#hou.pwd().cook(force=True)
+import uuid
+id = "Unique ID is {}".format(uuid.uuid4())
+#print(id)
+start_x = 10
+start_y = 100
+image = np.zeros((858,2048,4),np.uint8)
+
+imgroot = hudObject.get_temp_path()
+if not os.path.exists(imgroot):
+    os.makedirs(imgroot)
+random_name = str(uuid.uuid4())
+imagepath = os.path.join(imgroot, random_name) + '.png' 
+print(imagepath)
+for i in data.items():
+    text = f'{i[0]} : {i[1]}'
+    cv2.putText(image, text, (start_x,start_y),cv2.FONT_HERSHEY_SIMPLEX,.7,(0,0,255,255))
+    start_y = start_y +22
+#for file in os.listdir(imgroot):
+#    os.remove(os.path.join(imgroot,file))
+    
+cv2.imwrite(imagepath,image)
+if os.path.exists(imagepath):
+    hudCamera.parm("fgimage").set(imagepath)
+
+print("end")
+
+
+
+
+
+
+
